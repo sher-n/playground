e8b9aecda5896f0d20a0fcdd14e2b55737941530
sher-n <sjanmathong@gmail.com>
2021-07-10 16:36:53 +0700

    add token no error

--- backend-express/controllers/todoList.js
+++ backend-express/controllers/todoList.js
@@ -2,18 +2,46 @@ const db = require('../models');
 
 
 const getTodoList = async (req,res) => {
-    const todoList = await db.TodoList.findAll()
+    const todoList = await db.TodoList.findAll({ where : { user_id : req.user.id}})
+    console.log(req.user)
     res.status(200).send(todoList);
 }
 const createTodoList = async (req,res) => {
     const newTodo = await db.TodoList.create({
         task : req.body.task,
-        user_id : 0 
+        user_id : req.user.id 
     })
     res.status(201).send(newTodo);
 }
 
+const deleteTodolist = async (req,res) => {
+    const targetId = Number(req.params.id);
+    const targetTodo = await db.TodoList.findAll({ where : { id : targetId, user_id : req.user.id}});
+    if (targetTodo) {
+        await targetTodo.destroy();
+        res.status(204).send();
+    } else {
+        res.status(404).send({ message : "todolist not found"});
+    }
+}
+
+const updateTodolist = async (req,res) => {
+    const targetId = Number(req.params.id);
+    const newTask = req.body.task;
+
+    const targetTodo = await db.TodoList.findOne({ where : { id : targetId, user_id : req.user.id}});
+    await targetTodo.update({
+        task : newTask
+    }, {
+        where : { id : targetId}
+    })
+    res.status(200).send({message : 'updating success'});
+    
+}
+
 module.exports = {
     getTodoList,
-    createTodoList
+    createTodoList,
+    deleteTodolist,
+    updateTodolist
 }
--- backend-express/routes/todoList.js
+++ backend-express/routes/todoList.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const TodolistControllers = require('../controllers/todoList');
+const passport = require('passport');
 
+const authentication = passport.authenticate('jwt', { session : false })
 
-router.get('/', TodolistControllers.getTodoList);
-router.post('/', TodolistControllers.createTodoList);
+router.get('/',authentication, TodolistControllers.getTodoList);
+router.post('/',authentication, TodolistControllers.createTodoList);
+router.delete('/:id',authentication, TodolistControllers.deleteTodolist);
+router.put('/:id', authentication, TodolistControllers.updateTodolist);
 
 module.exports = router;
