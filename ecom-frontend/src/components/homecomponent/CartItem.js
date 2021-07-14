8865ed527dc7290773a9ebb5b71378973b560363
sher-n <sjanmathong@gmail.com>
2021-07-14 16:31:24 +0700

    home page 30%

--- ecom-frontend/src/components/homecomponent/CartItem.js
+++ ecom-frontend/src/components/homecomponent/CartItem.js
@@ -3,12 +3,13 @@ import './CartItem.css';
 
 function CartItem() {
     return (
-        <div id ='container'> 
+        <div id='container'> 
+            <div className="card-container">test</div>
             {/* <div className='image-container'>
             <img src='https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-12-pro-family-hero?wid=940&hei=1112&fmt=jpeg&qlt=80&.v=1604021663000' alt='product' />
             </div>
             <div className='detail-container'>
-                <div>name</div>
+                <div>nammmme</div>
                 <div>price</div>
                 <div>button</div>
             </div>
